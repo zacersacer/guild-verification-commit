a2401ce888a1531dbfd0ee970a49069d43a67756
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:41:53 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:41:50.570Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-05T23:41:52.169Z
+Commit: Patch API endpoint error handling
