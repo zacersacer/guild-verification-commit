0f3d6112d2d8f19135e52b9e4baabc6e50569b52
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:29:12 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:26:11.541Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T23:29:11.042Z
+Commit: Patch API endpoint error handling
