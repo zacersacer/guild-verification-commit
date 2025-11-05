3931d9da5c71d9cc82140859ec4e258747222039
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:39:11 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:38:51.866Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T23:39:10.527Z
+Commit: Resolve null pointer exception in user service
