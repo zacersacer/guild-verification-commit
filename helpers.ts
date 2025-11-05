89ee65c8e3568f89898a17baa210667cb19e083a
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:29:52 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:29:05.635Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T23:29:50.663Z
+Commit: Patch API endpoint error handling
