ff054a0e57fe39b37e16fc57ad4c97edcd061f1c
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:39:15 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:38:54.912Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T23:39:14.045Z
+Commit: Fix broken links in documentation
