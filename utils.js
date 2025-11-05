c380ab1021c9a53802c708187b69a859d63914e3
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:40:49 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:40:46.538Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T23:40:47.937Z
+Commit: Fix CSS styling bug on mobile devices
