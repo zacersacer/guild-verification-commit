8117abca7077c699d5d2ca156946d485fbf2bb16
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:25:54 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:25:48.423Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-05T23:25:53.536Z
+Commit: Add multi-language support
