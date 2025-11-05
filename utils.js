aae10c11d82c9ce19f0d88be9d88a744e43b80be
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:25:14 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:25:06.803Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T23:25:12.603Z
+Commit: Resolve null pointer exception in user service
