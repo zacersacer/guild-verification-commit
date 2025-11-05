23f8992fde0c0824fce8bf899bafa791a37fdcc9
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:40:42 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:39:22.657Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-05T23:40:40.855Z
+Commit: Resolve null pointer exception in user service
