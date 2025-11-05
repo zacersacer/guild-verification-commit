2e12eb0d0adae85321bf14cd8f60144927667cc5
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:41:35 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:40:28.291Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T23:41:33.853Z
+Commit: Resolve null pointer exception in user service
