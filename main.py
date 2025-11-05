e6f1cacc5fce0d7554029386240801e68419a096
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:33:27 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:33:18.127Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T23:33:26.003Z
+Commit: Fix race condition in async operations
