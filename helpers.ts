9bea22f3849e5179c63baa4b03daf97ad83c8c3f
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:32:29 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:32:13.006Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T23:32:28.268Z
+Commit: Introduce real-time notifications
