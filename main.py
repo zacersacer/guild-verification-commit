dd10d36f5b256973f2276c0c062f7e578ece4dcc
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:25:59 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:25:34.720Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T23:25:58.568Z
+Commit: Fix memory leak in event handlers
