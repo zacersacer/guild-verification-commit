02af6a8ecdd272e5901e13b40c19ae687201a591
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:24:12 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:24:09.584Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T23:24:11.034Z
+Commit: Patch security vulnerability in authentication
