ebf150be4d87907eeb34fe4fe34444f2be7aa467
acers <113265542+zacersacer@users.noreply.github.com>
2025-11-06 02:25:25 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:25:12.603Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-05T23:25:24.491Z
+Commit: Add multi-language support
